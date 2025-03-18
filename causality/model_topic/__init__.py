ce9368866432e08e08bf1639b90ab2f4d78a4700
Pataraphorn <mpataraphorn@gmail.com>
2025-03-18 17:34:03 +0700

    data_col for bert

--- causality/model_topic/__init__.py
+++ causality/model_topic/__init__.py
@@ -8,11 +8,15 @@ class topicModel:
         self.topic_type = topic_type
         self.topic_dict = None
         self.df = pd.DataFrame()
+        self.data_col = "content"
         print("Processing with topic modeling name:", self.topic_type)
         if self.topic_type == 'BERTopic':
             print(f'Start Initialized')
         else:
             print(f'We do not have the {topic_type} mode yet.')
+
+    def set_data_col(self, data_col: str):
+        self.data_col = data_col
 
     def topic_from_trained_model(self, model_path: str, embedding_model: str ="all-MiniLM-L6-v2"):    
         if self.topic_type == 'BERTopic':
@@ -28,7 +32,9 @@ class topicModel:
         if self.topic_type == 'BERTopic':
             if embedding_path is None:
                 embedding_path = bert.run_embedding(df, batch_size=batch_size)
-            self.model = bert.update_model(df, data_col='content', embeddings_load=embedding_path)
+            self.model = bert.update_model(
+                df, data_col=self.data_col, embeddings_load=embedding_path
+            )
             self.topic_dict = self.get_dict_of_topic()
             # self.topic_pos = BERT.get_coor_topic(self)
             print(f"Finish training {self.topic_type} model of topics")
@@ -43,7 +49,7 @@ class topicModel:
 
     def get_topic(self, df: pd.DataFrame, map: dict = None):
         if map:
-            df['topic'] = df['content'].progress_apply(lambda x: map(x))
+            df["topic"] = df[self.data_col].progress_apply(lambda x: map(x))
         elif self.topic_type == 'BERTopic':
             df["topic"] = self.model.topics_
         else:
